3ab082459a2aa09686a7499a28b469e700cd50e3
harenick <raphael.cdias@outlook.com>
2019-10-16 08:07:36 -0300

    first commit

---
+++ App3.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Agora Vai ',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>Meu primeiro app, só alegria</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ ander.js
@@ -0,0 +1,15 @@
+import { write } from "fs";
+
+// deosntração de como ler variaveis ao contrario
+//
+// autor raphael dias
+//
+
+
+let ander = [1,2,3] 
+
+let x  = 3;
+for ( x = 3; x > 1; x--){
+  write(ander[x])
+
+}
